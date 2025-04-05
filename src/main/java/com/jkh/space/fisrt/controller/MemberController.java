37b1726378a705fc5e00666ec09bd680859674cf
NewJKH <newsun0805@gmail.com>
2025-04-05 13:50:10 +0900

    first

--- src/main/java/com/jkh/space/fisrt/controller/MemberController.java
+++ src/main/java/com/jkh/space/fisrt/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.jkh.space.fisrt.controller;
 
+import com.jkh.space.fisrt.domain.member.Member;
 import com.jkh.space.fisrt.domain.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -19,5 +21,15 @@ public class MemberController {
         return "members/createMembers";
     }
 
+    /**
+     * @param form POST 방식으로 값을 받음
+     * @return 리 다이렉트를 해서 다시 홈 페이지로 이동
+     * */
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
 
 }
