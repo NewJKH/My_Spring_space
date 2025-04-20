9ab7db3301bbb37453ec3fc0afdf77461e24893e
NewJKH <newsun0805@gmail.com>
2025-04-20 11:50:56 +0900

    first

---
+++ src/main/java/com/jkh/space/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.jkh.space.service;
+
+import com.jkh.space.domain.member.Member;
+import com.jkh.space.repository.MemberRepository;
+import com.jkh.space.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * @param member 회원
+     * @return 회원가입한 ID 반환
+     */
+    public Long join(Member member) {
+        // 중복회원은 거절
+        validateDuplicate(member); // 중복 회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicate(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findMember(Long id){
+        return memberRepository.findById(id);
+    }
+}
---
+++ src/test/java/com/jkh/space/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package com.jkh.space.service;
+
+import com.jkh.space.domain.member.Member;
+import com.jkh.space.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MemberServiceTest {
+    private MemoryMemberRepository memberRepository;
+    private MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() { // 생성자 느낌?
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // g iven
+        Member m1 = new Member(0L,"Hello");
+        Member m2 = new Member(1L,"Hello"); // 중복회원 검증을 위해서 동일한 이름을 사용
+
+        // w hen
+        Long m1Id = memberService.join(m1);
+        Long m2Id = memberService.join(m2);
+
+
+        // t hen
+        Member member1 = memberService.findMember(m1Id).get();
+        Member member2 = memberService.findMember(m2Id).get();
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+
+    }
+
+    @Test
+    void findMembers() {
+
+    }
+
+    @Test
+    void findMember() {
+    }
+}
