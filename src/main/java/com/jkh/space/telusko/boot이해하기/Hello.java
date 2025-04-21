31c0928b3033ed5a440d8e8596b7cf3d467f3f8b
NewJKH <newsun0805@gmail.com>
2025-04-21 15:56:28 +0900

    update #8 EP 포트 나누기

--- src/main/java/com/jkh/space/telusko/boot이해하기/Hello.java
+++ src/main/java/com/jkh/space/telusko/boot이해하기/Hello.java
@@ -1,11 +1,8 @@
 package com.jkh.space.telusko.boot이해하기;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+//@RestController
 public class Hello {
-    @RequestMapping("/")
+//    @RequestMapping("/")
     public String greet() {
         return "Hello World, Welcome";
     }
---
+++ src/main/java/com/jkh/space/telusko/web만들기/ServerApp.java
@@ -0,0 +1,13 @@
+package com.jkh.space.telusko.web만들기;
+
+import com.jkh.space.SpaceApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceApplication.class, args);
+
+    }
+}
