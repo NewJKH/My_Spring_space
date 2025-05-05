f67c185b57c45a52d09e173107bd1c3692dac98e
NewJKH <newsun0805@gmail.com>
2025-05-05 16:28:40 +0900

    add create Memo

--- src/main/java/com/jkh/space/memo/entity/dto/MemoResponseDto.java
+++ src/main/java/com/jkh/space/memo/entity/dto/MemoResponseDto.java
@@ -1,8 +1,15 @@
 package com.jkh.space.memo.entity.dto;
 
+import com.jkh.space.memo.entity.memo.Memo;
+
 public class MemoResponseDto {
     private Long id;
     private String title;
     private String content;
 
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.title = memo.getTitle();
+        this.content = memo.getContent();
+    }
 }
